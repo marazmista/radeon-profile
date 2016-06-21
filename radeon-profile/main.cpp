d77d5fc150e4e805e7c62b1c19726086e39f6e71
Daniele Santini <danysan95@gmail.com>
2016-06-21 16:20:23 +0200

    Improved translation file detection

--- radeon-profile/main.cpp
+++ radeon-profile/main.cpp
@@ -1,15 +1,23 @@
 #include "radeon_profile.h"
 #include <QApplication>
 #include <QTranslator>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QTranslator translator;
-    if(translator.load(QLocale(), "strings", ".") || translator.load(QLocale(), "strings", ".", "/usr/share/radeon-profile"))
+    QTranslator translator(&a);
+    if (translator.load(QLocale(), "strings", ".")){
+        qDebug() << "Translation found in in the current path";
         a.installTranslator(&translator);
-    else
+    } else if (translator.load(QLocale(), "strings", ".", QApplication::applicationDirPath())){
+        qDebug() << "Translation found in in the executable directory";
+        a.installTranslator(&translator);
+    } else if (translator.load(QLocale(), "strings", ".", "/usr/share/radeon-profile")){
+        qDebug() << "Translation found in /usr/share/radeon-profile";
+        a.installTranslator(&translator);
+    } else
         qWarning() << "Failed loading translation";
 
     radeon_profile w(a.arguments());
